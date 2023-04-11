f6bda224ae87e53e09b1d851eaad51dbb2512d8b
Harman-454 <harman.marwah@outlook.com>
2023-04-11 17:02:58 +0200

    commit

--- a1.py
+++ a1.py
@@ -1,3 +1,16 @@
-print("Hello World")
-print("Goodbye sad world cause sam sucks")
-print(":)")
+import socket
+
+from _thread import *
+import threading
+
+server_ip = "143.47.184.219"
+server_port = "5378"
+
+s = socket.socket()
+
+print(f"[*] Connecting to {server_ip}:{server_port}...")
+
+s.connect((server_ip, server_port))
+print("[+] Connected.")
+
+name = input("Enter your name: ")
