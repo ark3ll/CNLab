c623e9ff195ad4d732dff821381862a8199bcb7f
Harman-454 <harman.marwah@outlook.com>
2023-04-16 15:44:52 +0200

    update

--- a1.py
+++ a1.py
@@ -1,16 +1,39 @@
 import socket
-
-from _thread import *
 import threading
 
-server_ip = "143.47.184.219"
-server_port = "5378"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s = socket.socket()
+#defines and connects to port
+server_port = ("143.47.184.219", 5378)
+s.connect(server_port)
 
-print(f"[*] Connecting to {server_ip}:{server_port}...")
+while True:
+    name = input("Enter Username: ")
+    string_bytes = ("HELLO-FORM" + name + "\n").encode("utf-8")
+    bytes_len = len(string_bytes)
+    num_bytes = bytes_len
+    while num_bytes > 0:
+        num_bytes -= s.send(string_bytes[bytes_len-num_bytes:]) 
 
-s.connect((server_ip, server_port))
-print("[+] Connected.")
+    data = s.recv(4096).decode()
+    print(data)
 
-name = input("Enter your name: ")
+    if data == "IN-USE\n":
+        s.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(server_port)
+
+    elif data == "BUSY/n":
+        print("Maximum number of clients has been reached, Try again later")
+        s.close
+    else:
+        break
+
+
+
+
+        
+
+    
+
+
