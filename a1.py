52ca5db26fcc2dd7db9c00b2c89c0b2e0e56e24a
Harman-454 <harman.marwah@outlook.com>
2023-04-16 16:56:22 +0200

    stuff

--- a1.py
+++ a1.py
@@ -8,52 +8,68 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_port = ("143.47.184.219", 5378)
 client.connect(host_port)
 
-def receive():
+# def receive():
+#     while True:
+#         try:
+#             bytes_len = len(string_bytes)
+#             num_bytes = bytes_len
+#             while num_bytes > 0:
+#             num_bytes -= client.send(string_bytes[bytes_len-num_bytes:]) 
+#             message = client.recv(1024).decode('ascii')
+#             if message == 'NICK':
+#                 client.send(username.encode('ascii'))
+#             else:
+#                 print(message)
+#         except:
+#             print("An error occurred!")
+#             client.close()
+#             break
+
+# def write():
+#         while True:
+#             message = f'{username}: {input("")}'
+#             client.send(message.encode('ascii'))
+
+# receive_thread = threading.Thread(target=receive)
+# receive_thread.start()
+
+# write_thread = threading.Thread(target=write)
+# write_thread.start()
+
+
+
+while True:
+    name = input("Enter Username: ")
+    string_bytes = ("HELLO-FORM" + name + "\n").encode("utf-8")
+    bytes_len = len(string_bytes)
+    num_bytes = bytes_len
+    while num_bytes > 0:
+        num_bytes -= client.send(string_bytes[bytes_len-num_bytes:]) 
+
+    data = client.recv(4096).decode()
+    print(data)
+
+    if data == "IN-USE\n":
+        client.close()
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(server_port)
+
+    elif data == "BUSY/n":
+        print("Maximum number of clients has been reached, Try again later")
+        client.close
+    else:
+        break
+
+def other(client):
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(username.encode('ascii'))
-            else:
-                print(message)
-        except:
-            print("An error occurred!")
-            client.close()
-            break
-def write():
-        while True:
-            message = f'{username}: {input("")}'
-            client.send(message.encode('ascii'))
+            print(client.recv(4096).decode())
+        
+        except OSError as msg:
+            print(msg)
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
-
-
-
-# while True:
-#     name = input("Enter Username: ")
-#     string_bytes = ("HELLO-FORM" + name + "\n").encode("utf-8")
-#     bytes_len = len(string_bytes)
-#     num_bytes = bytes_len
-#     while num_bytes > 0:
-#         num_bytes -= s.send(string_bytes[bytes_len-num_bytes:]) 
-
-#     data = s.recv(4096).decode()
-#     print(data)
-
-#     if data == "IN-USE\n":
-#         s.close()
-#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         s.connect(server_port)
-
-#     elif data == "BUSY/n":
-#         print("Maximum number of clients has been reached, Try again later")
-#         s.close
-#     else:
-#         break
+t = threading.Thread(daemon=True, target=other, args=(client,))
+t.start()
 
 
 
@@ -62,6 +78,6 @@ write_thread.start()
 
     
 
-# s.connect((server_ip, int(server_port)))
+# client.connect((server_ip, int(server_port)))
 # print("[+] Connected.")
 
